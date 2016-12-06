86a31fdb1714bcdb6c837ee5dfea51a1232698ad
Luke Barbuto <luke.barbuto@gmail.com>
2016-12-06 14:49:48 -0800

    Update graphql schema to query todos

--- web/schema.ex
+++ web/schema.ex
@@ -1,23 +1,25 @@
 defmodule TodoApp.Schema do
   use Absinthe.Schema
-
-  # Example data
-  @items %{
-    "foo" => %{id: "foo", name: "Foo"},
-    "bar" => %{id: "bar", name: "Bar"}
-  }
+  alias TodoApp.{Repo, Todo}
 
   query do
+    field :items, list_of(:item) do
+      resolve fn _, _ ->
+        {:ok, Todo |> Repo.all}
+      end
+    end
+
     field :item, :item do
       arg :id, non_null(:id)
       resolve fn %{id: item_id}, _ ->
-        {:ok, @items[item_id]}
+        {:ok, Todo |> Repo.get(item_id)}
       end
     end
   end
 
   object :item do
     field :id, :id
-    field :name, :string
+    field :title, :string
+    field :completed, :boolean
   end
 end
