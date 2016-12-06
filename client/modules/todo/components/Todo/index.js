19e47628cf69ea94573952aef806e20d281597b7
Luke Barbuto <luke.barbuto@gmail.com>
2016-12-06 09:37:55 -0800

    Set up theme for todo

--- client/modules/todo/components/Todo/index.js
+++ client/modules/todo/components/Todo/index.js
@@ -1,25 +1,42 @@
 /* @flow */
 import React from 'react'
 
+import { themr } from 'react-css-themr';
 import Checkbox from 'react-toolbox/lib/checkbox/Checkbox'
 import Input from 'react-toolbox/lib/input/Input'
 
-export default function Todo(
-  { completed, text, onChange, onToggle }: {
+import todoTheme from './theme.scss'
+
+type TodoTheme = {
+  checkbox: string,
+  input: string,
+  todo: string,
+}
+
+function Todo(
+  { completed, text, theme, onChange, onToggle }: {
     completed?: boolean,
     text?: string,
+    theme: TodoTheme,
     onChange: () => void,
     onToggle: () => void
   }
 ) {
   return (
-    <div>
-      <Checkbox
-          checked={completed}
-          onChange={onToggle} />
-      <Input
-          defaultValue={text}
-          onChange={onChange} />
+    <div className={theme.todo}>
+      <div className={theme.checkbox}>
+        <Checkbox
+            checked={completed}
+            onChange={onToggle} />
+      </div>
+
+      <div className={theme.input}>
+        <Input
+            defaultValue={text}
+            onChange={onChange} />
+      </div>
     </div>
   )
 }
+
+export default themr('', todoTheme)(Todo)
