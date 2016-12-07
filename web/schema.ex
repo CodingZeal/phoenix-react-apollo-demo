985049c9bcc5e145d918ec0b69209de19244aed2
Luke Barbuto <luke.barbuto@gmail.com>
2016-12-06 16:03:42 -0800

    Rename instances of item to todo

--- web/schema.ex
+++ web/schema.ex
@@ -3,21 +3,21 @@ defmodule TodoApp.Schema do
   alias TodoApp.{Repo, Todo}
 
   query do
-    field :items, list_of(:item) do
+    field :todos, list_of(:todo) do
       resolve fn _, _ ->
         {:ok, Todo |> Repo.all}
       end
     end
 
-    field :item, :item do
+    field :todo, :todo do
       arg :id, non_null(:id)
-      resolve fn %{id: item_id}, _ ->
-        {:ok, Todo |> Repo.get(item_id)}
+      resolve fn %{id: todo_id}, _ ->
+        {:ok, Todo |> Repo.get(todo_id)}
       end
     end
   end
 
-  object :item do
+  object :todo do
     field :id, :id
     field :title, :string
     field :completed, :boolean
