a95a65df2fff60ff9d38e7575930ea796ecbb8a2
Luke Barbuto <luke.barbuto@gmail.com>
2016-12-12 19:41:14 -0800

    Configure assets loading in production

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,7 +1,21 @@
 defmodule TodoApp.LayoutView do
   use TodoApp.Web, :view
 
-  def bundle_path do
-    Application.get_env(:todo_app, TodoApp.LayoutView)[:bundle_path]
+  @asset_manifest "priv/static/asset-manifest.json"
+    |> File.read!
+    |> Poison.Parser.parse!
+
+  def client_path do
+    case Application.get_env(:todo_app, TodoApp.LayoutView) do
+      [client_path: client_path] -> client_path
+      _ -> @asset_manifest["main.js"]
+    end
+  end
+
+  def styles_path do
+    case Application.get_env(:todo_app, TodoApp.LayoutView) do
+      [styles_path: styles_path] -> styles_path
+      _ -> @asset_manifest["main.css"]
+    end
   end
 end
