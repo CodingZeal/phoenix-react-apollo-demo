65638934d2b140acbddb2033b0b7994a78ec5a5f
Luke Barbuto <luke.barbuto@gmail.com>
2016-12-12 20:52:57 -0800

    Configure for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,18 @@ use Mix.Config
 # which you typically run after static files are built.
 config :todo_app, TodoApp.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80]
+  url: [scheme: "https",
+        host: "phoenix-react-apollo-demo.herokuapp.com",
+        port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Database config
+config :todo_app, TodoApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -57,4 +68,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
