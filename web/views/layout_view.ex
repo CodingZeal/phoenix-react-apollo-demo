1e05bc57801f185a3c0d5bf8478aec3013e0dc2a
Luke Barbuto <luke.barbuto@gmail.com>
2016-12-22 11:14:05 -0800

    Don't raise when asset manifest is not available

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,21 +1,11 @@
 defmodule TodoApp.LayoutView do
   use TodoApp.Web, :view
 
-  @asset_manifest "priv/static/asset-manifest.json"
-    |> File.read!
-    |> Poison.Parser.parse!
-
   def client_path do
-    case Application.get_env(:todo_app, TodoApp.LayoutView) do
-      [client_path: client_path] -> client_path
-      _ -> @asset_manifest["main.js"]
-    end
+    Application.get_env(:todo_app, TodoApp.LayoutView)[:client_path]
   end
 
   def styles_path do
-    case Application.get_env(:todo_app, TodoApp.LayoutView) do
-      [styles_path: styles_path] -> styles_path
-      _ -> @asset_manifest["main.css"]
-    end
+    Application.get_env(:todo_app, TodoApp.LayoutView)[:styles_path]
   end
 end
